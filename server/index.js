c32b0de40c6a51aadeb298eba257f1d13f39d8bc
Shreya Agrawal <shreyanagrawal128@gmail.com>
2026-05-06 15:18:42 +0530

    Project Initialization

---
+++ server/index.js
@@ -0,0 +1,10 @@
+require('dotenv').config
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+
+const port = process.env.PORT || 3000;
+app.listen(port,()=>{
+    console.log("Server is running");
+})
