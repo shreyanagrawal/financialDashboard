ab23dac05668c9f7c2c1dc2cc4c93d2d0fa852e8
mahindra-07 <mahindramahi.g@gmail.com>
2026-05-07 20:42:31 +0530

    integrated registration in same login page

--- financialDashboard/src/App.jsx
+++ financialDashboard/src/App.jsx
@@ -1,6 +1,5 @@
 import {BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from "./pages/Login";
-import Register from "./pages/Registration";
 
   function App() {
       return (
