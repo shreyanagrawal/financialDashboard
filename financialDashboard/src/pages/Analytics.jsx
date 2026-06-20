c3de7c8348da707b7159253b11f7f6592f2f347d
Tejas <tejassangale90@gmail.com>
2026-06-20 13:19:21 +0530

    added chevronDown from lucide-react

--- financialDashboard/src/pages/Analytics.jsx
+++ financialDashboard/src/pages/Analytics.jsx
@@ -5,6 +5,7 @@ import { getAggregatedData, getAmountbyCategory, getTransactionsData } from '../
 import PlaidStats from '../components/PlaidStats';
 import AnalyticsChart from '../components/AnalyticsChart';
 import CategoryWiseData from '../components/CategoryWiseData';
+import { ChevronDown } from 'lucide-react';
 
 const Analytics = (userId) => {
     const {loading, setLoading} = useContext(AuthContext);
@@ -125,9 +126,7 @@ const Analytics = (userId) => {
                             ))}
                         </select>
                         <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-4 text-gray-500">
-                            <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/>
-                            </svg>
+                            <ChevronDown className="h-4 w-4"/>
                         </div>
                         </div>
                     </div>
